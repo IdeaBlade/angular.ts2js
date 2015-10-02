bff7b96ecba97c620d3d8942fd9f827f60492647
Jay Traband <jtraband@comcast.net>
2015-10-02 15:13:59 -0700

    incorporate tsc

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ var gulp = require('gulp');
 var taskListing = require('gulp-task-listing');
 var path = require('canonical-path');
 var _ = require('lodash');
+var argv = require('yargs').argv;
+var exec = require('child_process').exec;
+var Q = require('q');
+var execPromise = Q.denodeify(exec);
+var tsc = require('gulp-typescript');
+var streamToPromise = require('stream-to-promise');
+var fs = require('fs');
 
 var angularVisitor = require('./angularVisitor');
 
@@ -13,17 +20,64 @@ gulp.task('help', taskListing.withFilters(function(taskName) {
   return shouldRemove;
 }));
 
-gulp.task('recast1', function() {
-  var sourceFile = './examples/simple/app.js';
-  var destFile = './examples/simple/app.rewrite.js';
-  output = angularVisitor.rewriteFile(sourceFile, destFile);
-  console.log(output);
+// if no args - compiles and rewrites all ts files in the examples/simple dir
+// if -f value ends in '.ts' then compiles all ts files and rewrites just the js version of the file specified.
+// if -f value ends in '.js' then just rewrites the js file.
+gulp.task('rewrite', function() {
+  var filePath = argv.file || argv.f || 'simple';
+  filePath = path.join('examples', filePath);
+  var fstat = fileStats(filePath);
+  if (fstat == null) {
+    console.log('unable to locate file or folder: ' + filePath);
+    return;
+  }
+  var dirName = path.dirname(filePath);
+  var extName = path.extname(filePath);
+  if (extName == '.ts') {
+    // convert to .js
+    filePath = path.join(dirName, path.basename(filePath, '.ts') + '.js');
+  }
+
+  if (fstat.isDirectory() || extName == '.ts') {
+    var folderPath = fstat.isDirectory() ? filePath : dirName;
+    tscCompile(folderPath).then(function () {
+      rewrite(filePath);
+    }).catch(function () {
+      rewrite(filePath);
+    })
+  } else if (extName == '.js') {
+    rewrite(filePath);
+  } else {
+    console.log('file path must end with ".js" or ".ts"');
+  }
+
 });
 
-gulp.task('recast', function() {
-  var sourceFolder = './examples/simple';
-  angularVisitor.rewriteFolder(sourceFolder);
+function tscCompile(folderPath) {
+  var tsProject = tsc.createProject(path.join(folderPath, 'tsconfig.json'));
+  var tsResult = gulp.src(path.join(folderPath, '/*.ts'))
+    .pipe(tsc(tsProject));
+  return streamToPromise(tsResult);
+}
 
-});
+function rewrite(filePath) {
+
+  var isDir = fs.lstatSync(filePath).isDirectory();
+
+  if (isDir) {
+    angularVisitor.rewriteFolder(filePath);
+  } else {
+    output = angularVisitor.rewriteFile(filePath);
+    console.log(output);
+  }
+}
+
+function fileStats(filePath) {
+  try   {
+    return fs.lstatSync(filePath);
+  } catch (err) {
+    return null;
+  }
+}
 
 gulp.task('default', ['help']);
